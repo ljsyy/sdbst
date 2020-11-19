2a9fcaa9dc2382cc8afed85e12f5e481d5f44bee
Lee <847787912@qq.com>
2020-11-19 17:39:57 +0800

    123

--- src/main/java/com/unifs/sdbst/app/controller/appinfo/APPinfoController.java
+++ src/main/java/com/unifs/sdbst/app/controller/appinfo/APPinfoController.java
@@ -24,6 +24,7 @@ public class APPinfoController {
     private AppinfoService appinfoService;
 
 
+
     @RequestMapping(value = "getCurrentVersion")
     @ResponseBody
     public String getCurrentVersion(AppVersion appVersion, HttpServletRequest request, HttpServletResponse response) {
